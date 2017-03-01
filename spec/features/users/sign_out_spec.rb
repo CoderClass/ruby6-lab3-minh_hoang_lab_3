9eb14bac79d9be16e294810be7f1de29f17b744a
Hoang Le <xh.dhhv@gmail.com>
2017-03-01 19:31:02 +0700

    hey minh where are you

--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -7,6 +7,7 @@ feature 'Sign out', :devise do
   before do
     @user = FactoryGirl.create(:user)
     signin(@user.email, @user.password)
+    puts "#{@user.email}, #{@user.password}"
   end
 
   scenario 'user is signed in' do
@@ -19,9 +20,13 @@ feature 'Sign out', :devise do
   #   Then I see a signed out message
   scenario 'user signs out successfully' do
     click_link 'Sign out'
-    fail "replace with your test here"
+    expect(page).to have_content "Signed out successfully."
+    # fail "replace with your test here minh map"
+
     # Add your test for this
     # HINT: test this step on the browser so you know what message to test.
     # HINT: identify the sign out message and test that.
+
+
   end
 end
