d768ad1908ac407550301b394052a260547d5bc1
Hoang Le <xh.dhhv@gmail.com>
2017-03-01 20:45:41 +0700

    co len minh oiiii

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,44 @@ class Product < ActiveRecord::Base
     all.order(:name)
   end
 
+  def discount_amount
+  	discount_percent = case price_vnd
+
+  	when 0..100000
+  		1
+  	when 100000..200000
+  		0.21
+  	when 200000..800000
+  		0.31
+  	else
+  		0.41
+  	end
+
+  	self.price_vnd = price_vnd * discount_percent
+  	self.save
+  end
+
+  def final_price
+ 	minh_map = self.price_vnd % 10000
+ 	
+ 	case minh_map
+ 	when 1000..4000
+ 		self.price_vnd = price_vnd - minh_map
+ 	when 5000..9000
+ 		self.price_vnd = price_vnd + minh_map
+ 	end
+ 	
+ 	save
+  end
+
+  def on_sale?
+  	
+  end
+
   private
 
   def set_default_image_path
     self.image_path ||= "http://lorempixel.com/200/200/fashion"
   end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,4 +18,59 @@ RSpec.describe Product, type: :model do
       expect(Product.alphabetical).to eq [a, b, c]
     end
   end
+
+
+  describe "#discount_amount" do
+    it "Hats that cost > 100,000 VND are going to be 21\% off." do
+      minh_map = Product.new(price_vnd: 150000)
+
+      minh_map.discount_amount
+      minh_map.save(validates: false)
+
+      expect(minh_map.price_vnd).to eq(150000 * 21 / 100)
+    end
+
+    it "Hats that cost t > 200,000 VND are going to be 31\% off." do
+      minh_map = Product.new(price_vnd: 250000)
+
+      minh_map.discount_amount
+      minh_map.save(validates: false)
+
+      expect(minh_map.price_vnd).to eq(250000 * 31 / 100)
+    end
+
+    it "Hats that cost > 800,000 VND are going to be 41\% off." do
+      minh_map = Product.new(price_vnd: 1000000)
+
+      minh_map.discount_amount
+      minh_map.save(validates: false)
+
+      expect(minh_map.price_vnd).to eq(1_000_000 * 41 / 100)
+    end
+  end
+
+  describe "#final_price" do
+    it "If the product cost 115,000 after discount, the final price should be 120,000." do
+      minh_map = Product.new(price_vnd: 115_000)
+
+      minh_map.final_price
+      puts "minh map #{minh_map.price_vnd}"
+      minh_map.save(validates: false)
+
+      expect(minh_map.price_vnd).to eq(120_000)
+    end
+
+    it "If the product cost 114,000 after discount, the final price should be 110,000." do
+      minh_map = Product.new(price_vnd: 114_000)
+
+      minh_map.final_price
+      minh_map.save(validates: false)
+
+      expect(minh_map.price_vnd).to eq(110_000)
+    end
+
+  end
+
 end
+
+
